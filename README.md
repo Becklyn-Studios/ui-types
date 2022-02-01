ba7209a049dbb065abe14ad838e70e9f4ebb54b3
Jesko Plitt <jp@becklyn.com>
2022-02-01 14:39:20 +0100

    Copy from mayd-components

---
+++ README.md
@@ -0,0 +1,14 @@
+Mayd UI Types
+=============
+
+[![CI](https://github.com/Becklyn-Studios/mayd-ui-types/actions/workflows/ci.yml/badge.svg)](https://github.com/Becklyn-Studios/mayd-ui-types/actions/workflows/ci.yml)
+
+
+Usage
+-----
+
+Install this library into your project:
+
+```shell
+npm i --save @mayd/ui-types
+```
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const StringType = "string";
+export const RteType = "rte";
+export const AssetType = "asset";
+export const LabeledLinkType = "labeledLink";
+
+export interface RelationType {
+    multiple: boolean;
+    data: Record<string, DataType>;
+}
+
+export type DataType = "string" | "rte" | "asset" | "labeledLink" | RelationType;
+
+export interface BaseComponentConfig<T extends {}> {
+    key: string;
+    Component: React.FC;
+    data: Record<string, DataType>;
+}
